773303ebacdf31e01ad6c4e46937228ab5439722
Romain <Romain@PC-Miron>
2017-05-15 15:49:34 +0200

    Plans : Aucune contrainte done, 1 contrainte done. Next : 2 contraintes

---
+++ PIDR/src/view/panels/OneConstraintChoicePanel.java
@@ -0,0 +1,125 @@
+package view.panels;
+
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
+import javax.swing.ButtonModel;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+
+import model.swing.Constraints;
+
+public class OneConstraintChoicePanel extends JPanel {
+	
+	private CardLayout cl;
+	
+	private ButtonGroup bG;
+	private ButtonModel bm;
+	
+	private JRadioButton pointChoice;
+	private JRadioButton slopeChoice;
+	
+	private JPanel constraintDatas;
+	private JPanel constraintChoice;
+	private JPanel pointChosen;
+	private VertexConstraintChoice vertexChosen;
+	
+	private JTextField x;
+	private JTextField y;
+	private JTextField z;
+	
+	public OneConstraintChoicePanel(Constraints c){
+		super(new GridLayout(1,2));
+		
+		cl = new CardLayout();
+		
+		constraintDatas = new JPanel(cl);
+		constraintChoice = new JPanel(new GridLayout(2,1));
+		
+		initPanelDatas(c);
+		
+		bG = new ButtonGroup();
+		pointChoice = new JRadioButton("Contraintes sur les coordonnées d'un point");
+		slopeChoice = new JRadioButton("Contraintes sur pente selon un vecteur d'un plan");
+		
+		bG.add(pointChoice);
+		bG.add(slopeChoice);
+		
+		pointChoice.setSelected(true);
+		
+		bm = bG.getSelection();
+		
+        setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black, 1)));
+
+        constraintChoice.add(pointChoice,"Point");
+        constraintChoice.add(slopeChoice,"Slope");
+        
+        addCustomListener(pointChoice);
+        addCustomListener(slopeChoice);
+        
+        this.add(constraintChoice);
+        this.add(constraintDatas);
+		
+	}
+	
+	public void initPanelDatas(Constraints c){
+		pointChosen = new JPanel(new GridLayout(3,2));
+		
+		x = new JTextField();
+		y = new JTextField();
+		z = new JTextField();
+		
+		pointChosen.add(new JLabel("Xw1 brut : "));
+		pointChosen.add(x);
+		pointChosen.add(new JLabel("Yw1 brut : "));
+		pointChosen.add(y);
+		pointChosen.add(new JLabel("Zw1 brut : "));
+		pointChosen.add(z);
+		
+		constraintDatas.add(pointChosen,"Point");
+		
+		vertexChosen = new VertexConstraintChoice(c);
+		
+		constraintDatas.add(vertexChosen,"Slope");
+	}
+	
+	public void addCustomListener(JRadioButton bouton){
+		bouton.addMouseListener(new MouseAdapter(){
+        	@Override
+        	public void mouseClicked(MouseEvent e) {
+        		super.mouseClicked(e);
+        		if (!bm.equals(bG.getSelection())){
+        			bm = bG.getSelection();
+        			if (bm.equals(pointChoice.getModel()))
+        			{
+        				cl.show(constraintDatas,"Point");
+        			}
+        			if (bm.equals(slopeChoice.getModel())){
+        				cl.show(constraintDatas, "Slope");
+        			}
+        		}
+
+        	}
+        });
+	}
+	
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("OneConstraintChoicePanel");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Constraints c = new Constraints();
+        OneConstraintChoicePanel newContentPane = new OneConstraintChoicePanel(c);
+        newContentPane.setOpaque(true);
+        frame.setContentPane(newContentPane);
+        frame.pack();
+        frame.setVisible(true);
+    }
+	
+}
---
+++ PIDR/src/view/panels/PlaneConstraintsChoicePanel.java
@@ -0,0 +1,14 @@
+package view.panels;
+
+import javax.swing.JPanel;
+
+import model.swing.Constraints;
+
+public class PlaneConstraintsChoicePanel extends JPanel {
+	//TODO : A remplir
+	
+	public PlaneConstraintsChoicePanel(Constraints c)
+	{
+		
+    }
+}

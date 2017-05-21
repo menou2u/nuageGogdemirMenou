bc5b4c64f128c5f01a17a3aac85d3efb68d30cfd
explnhb <bhugel57@gmail.com>
2017-05-21 19:07:11 +0200

    oin oin le oin

--- PIDR/src/model/swing/LineGraph.java
+++ PIDR/src/model/swing/LineGraph.java
@@ -1,62 +1,79 @@
 package model.swing;
 
 import java.awt.Component;
+import java.awt.Container;
+import java.util.LinkedList;
 
-import org.jzy3d.chart.Chart;
+import javax.swing.JFrame;
+
+import org.jzy3d.analysis.AbstractAnalysis;
 import org.jzy3d.chart.factories.AWTChartComponentFactory;
 import org.jzy3d.chart.factories.IChartComponentFactory;
 import org.jzy3d.colors.Color;
-import org.jzy3d.colors.ColorMapper;
-import org.jzy3d.colors.colormaps.ColorMapRainbow;
-import org.jzy3d.maths.Range;
-import org.jzy3d.plot3d.builder.Builder;
-import org.jzy3d.plot3d.builder.Mapper;
-import org.jzy3d.plot3d.builder.concrete.OrthonormalGrid;
-import org.jzy3d.plot3d.primitives.Shape;
+import org.jzy3d.maths.Coord2d;
+import org.jzy3d.plot3d.primitives.Scatter;
 import org.jzy3d.plot3d.rendering.canvas.Quality;
 
-public class LineGraph {
+public class LineGraph extends AbstractAnalysis{
 
 	private Component canvas;	
+	private LinkedList<Double> listX;
+	private LinkedList<Double> listY;
+	
+	public LineGraph(LinkedList<Double> listX, LinkedList<Double> listY) {
+		this.listX = listX;
+		this.listY = listY;
+	}
 	
 	public LineGraph() {
-		// Define a function to plot
-        Mapper mapper = new Mapper() {
-            @Override
-            public double f(double x, double y) {
-                //return x * Math.sin(x * y);
-            	return 0;
-            }
-        };
-
-        // Define range and precision for the function to plot
-        Range range = new Range(1, 5);
-        int steps = 80;
-
-        // Create the object to represent the function over the given range.
-        final Shape surface = Builder.buildOrthonormal(new OrthonormalGrid(range, steps, range, steps), mapper);
-        surface.setColorMapper(new ColorMapper(new ColorMapRainbow(), surface.getBounds().getZmin(), surface.getBounds().getZmax(), new Color(1, 1, 1, .5f)));
-        surface.setFaceDisplayed(true);
-        surface.setWireframeDisplayed(false);
-
-        // Create a chart
-        Chart chart = new Chart();
-        chart = AWTChartComponentFactory.chart(Quality.Advanced, IChartComponentFactory.Toolkit.swing);
-        chart.getScene().getGraph().add(surface);
-        
-        chart.addMouseCameraController();
-        chart.addMousePickingController(2);
-        /*AWTCameraMouseController controller = new AWTCameraMouseController(chart);
-
-		addMouseListener(controller);
-		addMouseMotionListener(controller);
-		addMouseWheelListener(controller);*/
-		canvas = (Component) chart.getCanvas();
+		listX = new LinkedList<>();
+		listY = new LinkedList<>();
+		init();
+	}
+	
+	public static void main(String[] args) {
+		LinkedList<Double> x = new LinkedList<>();
+		x.add(1.0);
+		x.add(3.0);
+		x.add(5.0);
+		LinkedList<Double> y = new LinkedList<>();
+		y.add(5.0);
+		y.add(9.0);
+		y.add(25.0);
+		LineGraph graph = new LineGraph(x, y);
+		graph.init();
 		
-		//JPanel panel = new JPanel();
-		//panel.add(canvas, BorderLayout.CENTER);
-		//return canvas;
-		//return new JPanel();
+		JFrame frame = new JFrame();
+		frame.setContentPane((Container) graph.getCanvas());
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+	}
+	
+	 public void init(){
+	        int size = listX.size();
+	        float x;
+	        float y;
+	        float z;
+	        float a;
+	        
+	        Coord2d[] points = new Coord2d[size];
+	        Color[]   colors = new Color[size];
+	        
+	        for(int i=0; i<size; i++){
+	            x = (float) ((double) listX.get(i));
+	            System.out.println("x "+x);
+	            y = (float) ((double) listY.get(i));
+	            System.out.println("y "+y);
+	            a = 0.25f;
+	            points[i] = new Coord2d(x, y);
+	            colors[i] = new Color(x, y, a);
+	        }
+	        
+	        Scatter scatter = new Scatter();
+	        chart = AWTChartComponentFactory.chart(Quality.Advanced, IChartComponentFactory.Toolkit.swing);
+	        chart.getScene().add(scatter);
+	        canvas = (Component) chart.getCanvas();
 	}
 
 	public Component getCanvas() {
